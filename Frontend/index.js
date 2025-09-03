9035d0cb3adc3c388df504e818fb28013f96df6d
Maria Helena Welk <welktech46@gmail.com>
2025-09-03 14:33:52 -0300

    commit

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,48 +1,65 @@
-fetch("http://localhost:3000/usuarios").then(res => {
+
+fetch("http://localhost:3000/usuarios").then(async res => {
+
     if (!res.ok) {
-        throw new Error("Erro ao buscar usuários");
+
+        throw new Error("erro na requisição...")
+
     }
 
-
-    return res.json();
+    return res.json()
 }).then(usuarios => {
-    const listaUsuarios = document.getElementById("lista-usuarios");
+
+    const listaUsuarios = document.querySelector("#lista-usuarios")
     usuarios.forEach(usuario => {
-        console.log(usuario.nome);
-        listaUsuarios.innerHTML +=
-            `<li class="list-group-item">
 
-        <div class="d-flex justify-content-between">
-            <h5>nome: ${usuario.nome} -idade ${usuario.idade}</h5>
-            <div>
-                <a href ="editarUsuario/index.html?id=${usuario.id}" class="btn btn-primary">Atualizar</a>
-                <button type="button" class="btn btn-danger" onclick="deletarUsuario(${usuario.id})">Deletar</button>
+        console.log(usuario.nome)
+        listaUsuarios.innerHTML += `
+        
+        <li class="list-group-item">
+        <div class = "d-flex justify-content-between">
+                <h5>nome: ${usuario.nome} - idade: ${usuario.idade}</h5>
+                <div>
+                    <a href="editarUsuario/index.html?id=${usuario.id}">
+                    <button type="button" class="btn btn-primary btn-atualizar">Atualizar</button></a>
+                    <button type="button" class="btn btn-danger" onclick="deletarUsuario(${usuario.id})">Deletar</button>
+                </div>
             </div>
-        </div>    
+        <li>`
 
-        </li>
-        `;
     });
+
+}).catch(err => {
+
+    console.log(err)
 
 })
-    .catch(err => {
-        console.error(err);
-    });
 
-    function deletarUsuario(userId) {
-        let confirmar = confirm("Você deseja excluir esse usuário" + userId + "?");
-        console.log(confirmar);
-        if (confirmar) {
-            fetch(`http://localhost:3000/usuarios/${userId}`, {
-    
-                method: 'DELETE',
-    
+function deletarUsuario(userId){
+
+    let confirmar = confirm(Você quer realmente excluir o Usuario ${userId}?)
+    if(confirmar){
+
+        fetch(`http://localhost:3000/usuarios/${userId}`, {
+        
+            method: 'DELETE',
+        
+        })
+        
+            .then(response => {
+                
+                if(response.ok){
+
+                    alert(Usuario ${userId} excluido com sucesso...)
+                    window.location.reload()
+                    return;
+                }
+                
+                alert("Algo deu errado...")
+        
             })
-    
-                .then(response => {
-                    console.log(response);
-                })
-    
-                .catch(error => console.log(error));
-        }
+        
+            .catch(error => console.log(error));
+
     }
+}
